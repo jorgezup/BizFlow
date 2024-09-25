4a88028eada3ba803ebce9881a23636f4915fb02
Jorge Zupirolli <jorgebertolo@gmail.com>
2024-09-24 23:42:38 -0300

    build

--- src/Application/Resources/ResourceManagerService.cs
+++ src/Application/Resources/ResourceManagerService.cs
@@ -5,9 +5,8 @@ namespace Application;
 
 public static class ResourceManagerService
 {
-    private static readonly ResourceManager ResourceManager = new ResourceManager("Application.Resources.Language", typeof(ResourceManagerService).Assembly);
+    private static readonly ResourceManager ResourceManager = new("Application.Resources.Language", typeof(ResourceManagerService).Assembly);
 
-    // Método para obter a string de recursos com base na chave e cultura
     public static string GetString(string key, string culture)
     {
         var cultureInfo = new CultureInfo(culture);
